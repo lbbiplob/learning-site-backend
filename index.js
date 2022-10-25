6b082fbea4072df507f77335c890995a36552899
lbbiplob <biplab.jsnu@gmail.com>
2022-10-26 01:42:15 +0600

    Category added

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors());
+
+const category = require('./data/categories.json')
+
+app.get('/category', (req , res)=>{
+    res.send(category)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
